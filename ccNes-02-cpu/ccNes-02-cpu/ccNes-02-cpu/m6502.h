f9dc3a0a3a715322d02f220092166f14a9073045
superchengzi <lou.cai@icloud.com>
2025-08-03 22:43:14 +0800

    Remove xcuserdata files

---
+++ ccNes-02-cpu/ccNes-02-cpu/ccNes-02-cpu/m6502.h
@@ -0,0 +1,13 @@
+//
+//  m6502.h
+//  ccNes-02-cpu
+//
+//  Created by cai on 2025/8/3.
+//
+
+#ifndef m6502_h
+#define m6502_h
+
+#include <stdio.h>
+
+#endif /* m6502_h */
---
+++ ccNes-02-cpu/ccNes-02-cpu/ccNes-02-cpu/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  ccNes-02-cpu
+//
+//  Created by cai on 2025/8/3.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
